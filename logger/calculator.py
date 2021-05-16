7a6f85df8b0699a455f7702e01cf1a7cf0d009c7
alxayeed <alxayeed@gmail.com>
2021-05-16 15:54:49 +0600

    logging using print

--- logger/calculator.py
+++ logger/calculator.py
@@ -1,12 +1,21 @@
+import logging
+
+
 def add(x, y):
+    """adds 2 numbers"""
+    print(f'adding {x} and {y}')
     return x + y
 
 def substract(x, y):
+    """substraction between 2 numbers"""
+    print(f'subtracting {y} from {x}')
     return x - y
 
 def multiply(x, y):
+    """multiplication of 2 numbers"""
     return x * y
 
 
 result = add(10, 5)
 print(result)
+print(substract(20, 7))
