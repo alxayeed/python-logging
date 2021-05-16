8d8510088dbe0713bc0a4a38d5da5bfa3b2afc23
alxayeed <alxayeed@gmail.com>
2021-05-16 15:57:08 +0600

    logging using logging module

--- logger/calculator.py
+++ logger/calculator.py
@@ -1,14 +1,17 @@
 import logging
 
+# logger configuration
+logging.basicConfig(level=logging.DEBUG)
+
 
 def add(x, y):
     """adds 2 numbers"""
-    print(f'adding {x} and {y}')
+    logging.debug(f'adding {x} and {y}')
     return x + y
 
 def substract(x, y):
     """substraction between 2 numbers"""
-    print(f'subtracting {y} from {x}')
+    logging.debug(f'subtracting {y} from {x}')
     return x - y
 
 def multiply(x, y):
