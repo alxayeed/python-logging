91691654acf6ce6f48b131dfaf96fcd0cd396bd0
alxayeed <alxayeed@gmail.com>
2021-05-17 11:45:27 +0600

    updated with logger instance

--- logger/calculator.py
+++ logger/calculator.py
@@ -10,23 +10,36 @@ CRITICAL
 
 
 # logger configuration
-logging.basicConfig(filename='logfile.log', level=logging.DEBUG,
-    format='%(asctime)s - line %(lineno)d in %(filename)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
+
+formatter = logging.Formatter(
+    '%(asctime)s - line %(lineno)d in %(filename)s - %(message)s')
+
+file_handler = logging.FileHandler('logfile.log')
+file_handler.setFormatter(formatter)
+
+log.addHandler(file_handler)
+# logging.basicConfig(filename='logfile.log', level=logging.DEBUG,
+#     format='%(asctime)s - line %(lineno)d in %(filename)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 
 
 def add(x, y):
     """adds 2 numbers"""
-    logging.info(f'adding {x} and {y}')
+    log.info(f'adding {x} and {y}')
     return x + y
+
 
 def substract(x, y):
     """substraction between 2 numbers"""
     logging.info(f'subtracting {y} from {x}')
     return x - y
 
+
 def multiply(x, y):
     """multiplication of 2 numbers"""
     return x * y
+
 
 def division(x, y):
     """divide a number with another"""
@@ -38,4 +51,4 @@ def division(x, y):
 
 result = add(10, 5)
 print(result)
-print(division(21, 0))
+# print(division(21, 0))
